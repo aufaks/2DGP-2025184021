e01406679de59b8ffd94d4b630e0fe62d4b9f796
aufaks <aufaks00@gmail.com>
2026-09-22 16:40:34 +0900

    캔버스 띄우기

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,4 +1,8 @@
 # 실습 과제 진행
+from pico2d import *
+
+open_canvas(800, 600)
+
 def move_circle():
     print('circle')
     pass
@@ -15,3 +19,5 @@ while True:
     move_circle()
     move_rectangle()
     move_triangle()
+
+close_canvas()
